c6b354ca730ae44ffa6700716791b1fbd1226b05
Adam Benichou <adbenichou@gmail.com>
2025-12-02 15:28:44 +0100

    routes final

---
+++ db/migrate/20251202141237_create_user_recipes.rb
@@ -0,0 +1,11 @@
+class CreateUserRecipes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :user_recipes do |t|
+      t.text :comment
+      t.references :user, null: false, foreign_key: true
+      t.references :recipe, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
