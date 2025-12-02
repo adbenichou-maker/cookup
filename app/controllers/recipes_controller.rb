59ebf7c604741a3646972465af17e3deb035ee74
Benjamin Schroeder <benjischroeder0@gmail.com>
2025-12-02 17:15:49 +0100

    dashboard and search

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,31 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def search
+    @query = params[:query]
+
+    @recipes = Recipe.all
+
+    # Search text
+    if @query.present?
+      @recipes = @recipes.where("title ILIKE ?", "%#{@query}%")
+    end
+
+    # Filter: difficulty level
+    if params[:level].present?
+      @recipes = @recipes.where(recipe_level: params[:level])
+    end
+
+    # Filter: time
+    if params[:time].present?
+      @recipes = @recipes.where("cooking_time <= ?", params[:time])
+    end
+  end
+
+end
