25ee6c0b4ad6c22fd2b1818c4fcfdb94da3cff3b
robertpniower <robert-pniower@gmx.de>
2025-12-03 14:49:39 +0100

    step skill view

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,6 +1,7 @@
 class SkillsController < ApplicationController
 
   def index
+    @skills = Skill.where(@skill.skill_level = params[:skill_level])
   end
 
   def show
--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -1,6 +1,9 @@
 class UserSkillsController < ApplicationController
 
   def create
+
+    @user_skill = UserSkill.new
+    @user_skill.safe
   end
 
 end
